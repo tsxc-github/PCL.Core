1aa715a8321f802d6dc2b9ad77fe91b0909deeef
tsxc <tsxc_personal@163.com>
2025-07-11 20:24:28 +0800

    Revert "imp(file-service): 服务停止时取消工作线程池"
    This reverts commit f5f0c0d52fa225061b8143ca81b71f89e3c35c12.

--- Service/FileService.cs
+++ Service/FileService.cs
@@ -72,8 +72,6 @@ public sealed class FileService : GeneralService
     private static LifecycleContext Context => _context!;
 
     private FileService() : base("file", "文件管理") { _context = ServiceContext; }
-
-    private static Thread? _fileLoadingThread;
     
     public override void Start()
     {
@@ -93,14 +91,13 @@ public sealed class FileService : GeneralService
 #endif
         // start load thread
         Context.Debug("正在启动文件加载守护线程");
-        _fileLoadingThread = NativeInterop.RunInNewThread(_FileLoadCallback, "Daemon/FileLoading");
+        NativeInterop.RunInNewThread(_FileLoadCallback, "Daemon/FileLoading");
     }
 
     public override void Stop()
     {
-        Context.Debug("正在停止文件处理工作");
+        Context.Debug("尝试停止文件处理工作");
         _running = false;
-        _fileLoadingThread?.Join();
     }
 
     #endregion
@@ -189,9 +186,6 @@ public sealed class FileService : GeneralService
                 }
             }
         }
-        
-        Context.Debug("尝试取消所有正在运行的工作");
-        threadPool.CancelAll();
     }
 
     /// <summary>
--- Utils/Threading/DualThreadPool.cs
+++ Utils/Threading/DualThreadPool.cs
@@ -18,7 +18,6 @@ public class DualThreadPool
 
     private readonly TaskFactory _ioFactory;
     private readonly TaskFactory _cpuFactory;
-    private readonly CancellationTokenSource _cts = new();
 
     /// <summary>
     /// 初始化 <see cref="DualThreadPool"/> 实例
@@ -33,17 +32,16 @@ public class DualThreadPool
 
         var ioScheduler = new LimitedConcurrencyLevelTaskScheduler(maxThread);
         var cpuScheduler = new LimitedConcurrencyLevelTaskScheduler(maxThread);
-        var cancellationToken = _cts.Token;
 
         // DenyChildAttach 防止子任务跑到外层 scheduler
         _ioFactory = new TaskFactory(
-            cancellationToken,
+            CancellationToken.None,
             TaskCreationOptions.DenyChildAttach,
             TaskContinuationOptions.None,
             ioScheduler);
 
         _cpuFactory = new TaskFactory(
-            cancellationToken,
+            CancellationToken.None,
             TaskCreationOptions.DenyChildAttach,
             TaskContinuationOptions.None,
             cpuScheduler);
@@ -68,9 +66,4 @@ public class DualThreadPool
     /// 提交一段异步 CPU 密集工作
     /// </summary>
     public Task QueueCpu(Func<Task> work) => _cpuFactory.StartNew(work).Unwrap();
-
-    /// <summary>
-    /// 取消所有正在执行的工作
-    /// </summary>
-    public void CancelAll() => _cts.Cancel();
 }

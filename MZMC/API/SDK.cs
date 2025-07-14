41d5f3fc3253351ab9afc13a6a16e496b0f71d50
tsxc <tsxc_personal@163.com>
2025-07-14 19:02:48 +0800

    feat: 用户登录

--- MZMC/API/SDK.cs
+++ MZMC/API/SDK.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using PCL.Core.MZMC;
-using Newtonsoft.Json;
-using RestSharp;
-using System.Security.Cryptography;
 using PCL.Core.MZMC.Helper;
+using RestSharp;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Security.Cryptography;
 
 namespace PCL.Core.MZMC.API
 {
 	public class SDK
 	{
 		private const string BaseUrl= "https://api.mzmc.top";
+
+		private const string ClientId = "5"; // 客户端ID
+		private const string ClientSecret = "oIZryQhWCuoKEG985nSNHALSoFnl4kBt"; // 客户端密钥
+		public const string RedirectUrl = "http://localhost:49287/callback/"; // Oauth回调地址
+		public static string OAuthUrl => $"{BaseUrl}/user/oauth2/authorize?client_id={ClientId}";
 
 		/// <summary>
 		/// 发送请求
@@ -29,10 +36,23 @@ namespace PCL.Core.MZMC.API
 		public class User
 		{
 
+            private long _id = -1;
 			private string _username = "";
-			private string _token = "";
+			private string _email = "";
+            private string _token = "";
 
-			public string Username{get => _username;}
+			private string _realname = "";
+            private string _nickname = "";
+
+
+
+            public string Username{get => _username;}
+
+            private void _SaveUserInfo()
+            {
+                Init.Config.AppSettings.Settings.Add("UserToken", this._token);
+                Init.Config.Save(ConfigurationSaveMode.Modified);
+            }
 
 			public Message IfLogin()
 			{
@@ -43,6 +63,9 @@ namespace PCL.Core.MZMC.API
 
 				if (message.OK == false)
 					_token = "";
+
+                _SaveUserInfo();
+
                 return message;
             }
 
@@ -59,18 +82,57 @@ namespace PCL.Core.MZMC.API
 
 				if (message.OK == true)
 				{
-					_username = username;
 					_token = message.Data.access_token.Value;
-				}
+                    GetProfile();
+                }
 
 				return new Message(message.OK, message.MessageText);
-			}
+            }
 
-			/// <summary>
-			/// 登出
-			/// </summary>
-			/// <returns>Message，无data</returns>
-			public Message Logout()
+            /// <summary>
+            /// 登录（Token）
+            /// </summary>
+            /// <param name="token">Token</param>
+            /// <returns>Message，不带Data</returns>
+            public Message LoginByToken(string token)
+            {
+                _token = token;
+                var message = GetProfile();
+
+                return new Message(message.OK, message.MessageText);
+            }
+
+            /// <summary>
+            /// 登录（OAuth）
+            /// 注意需要通过OAuth提前获得code才能使用该登录方式
+            /// </summary>
+            /// <param name="code"></param>
+            /// <returns></returns>
+            public Message OAuthLogin(string code)
+            {
+	            var request = new RestRequest("/user/oauth2/token", Method.Post);
+	            request.AddJsonBody(new { client_id = ClientId, client_secret = ClientSecret, code = code });
+	            var client = new RestClient(BaseUrl);
+	            var result = client.Execute(request);
+	            if(result.ResponseStatus==ResponseStatus.Error||result.Content.Contains("error"))
+		            return new Message(false,result.Content);
+	            dynamic content=JsonConvert.DeserializeObject(result.Content);
+	            var message = new Message(result.IsSuccessStatusCode,"操作成功",content);
+
+	            if (message.OK == true)
+	            {
+		            _token = message.Data.access_token.Value;
+		            GetProfile();
+	            }
+
+	            return new Message(message.OK, message.MessageText);
+            }
+
+            /// <summary>
+            /// 登出
+            /// </summary>
+            /// <returns>Message，无data</returns>
+            public Message Logout()
 			{
 				if (IfLogin().OK == false)
 					return new Message(false, "未登录");
@@ -86,6 +148,16 @@ namespace PCL.Core.MZMC.API
                     return new Message(false, "未登录");
 				var request = new RestRequest("/player/profile", Method.Get).AddHeader("Authorization", $"Bearer {_token}");
 				var message = Request(request);
+
+				if (message.OK == true)
+                {
+                    _id = message.Data.id.Value;
+                    _username = message.Data.username.Value;
+                    _realname = message.Data.realname.Value;
+                    _nickname = message.Data.nickname.Value;
+                    _email = message.Data.email.Value;
+
+                }
 				return message;
 			}
 
--- MZMC/Init.cs
+++ MZMC/Init.cs
@@ -1,13 +1,19 @@
 using PCL.Core.MZMC.Helper;
+using PCL.Core.MZMC.API;
+using System.Configuration;
 
 namespace PCL.Core.MZMC
 {
     public class Init
     {
+        public static Configuration Config;
+        public static SDK.User User = new SDK.User();
         public static void Main()
         {
             // 初始化
-
+            Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if(Config.AppSettings.Settings["UserToken"]!=null)
+                User.LoginByToken(Config.AppSettings.Settings["UserToken"].Value);
         }
     }
 }
